e293e54ab01e5cfb1bb1eea4b155558e70a3c91d
Nikolai Perlt <nikolaiperlt@gmail.com>
2021-09-26 13:42:43 +0200

    stuff

--- 1.semester/algorithmAndDesign/assigments/kattis/dynamicProgramming/weightedIntervalScheduling/program.py
+++ 1.semester/algorithmAndDesign/assigments/kattis/dynamicProgramming/weightedIntervalScheduling/program.py
@@ -1,6 +1,6 @@
 import fileinput
 import sys
-
+import math
 
 def main():
     data = get_formatted_data()
@@ -16,25 +16,37 @@ def solve(intervals: list) -> int:
 
     for i in range(n):
         s, _, w = intervals[i]
-        if i <= 0:
+        if i == 0:
             M[i] = 0
         elif i == 1:
             M[i] = w
         else:
-            x = 2
+            x = binary_search(i, intervals, s, i // 2, i)
 
-            for j in range(2, i + 1):
-                _, f, _ = intervals[i - j]
-                if s >= f:
-                    x = j
-                    break
-
-            take: int = w + M[i - x]
+            take = w if x == -1 else w + M[x]
             drop: int = 0 + M[i - 1]
 
             M[i] = max(take, drop)
 
     return M[n - 1]
+
+
+def binary_search(start_index: int, intervals: list, start_time: int, lookup_index: int, previous_index: int = 0):
+    lookup = intervals[lookup_index]
+    _, finish_time, _ = lookup
+
+    if (finish_time > start_time):
+        if(lookup_index == 0):
+            return -1
+
+        return binary_search(start_index, intervals, start_time, lookup_index // 2, lookup_index)
+    elif (finish_time < start_time):
+        if (start_index != lookup_index + 1 and intervals[lookup_index + 1][1] > start_time):
+            return lookup_index
+
+        return binary_search(start_index, intervals, start_time, math.ceil((lookup_index + previous_index) / 2), lookup_index)
+    else:
+        return lookup_index
 
 
 def get_formatted_data() -> list:
@@ -45,7 +57,8 @@ def get_formatted_data() -> list:
     interval_list = []
 
     for _ in range(n):
-        interval_list.append([int(x) for x in data_input.readline().strip().split(" ")])
+        interval_list.append(
+            [int(x) for x in data_input.readline().strip().split(" ")])
 
     return interval_list
 
--- 1.semester/pcpp/lectures/week03/Exercises/app/src/main/java/exercises03/TestCountPrimesThreads.java
+++ 1.semester/pcpp/lectures/week03/Exercises/app/src/main/java/exercises03/TestCountPrimesThreads.java
@@ -1,5 +1,6 @@
 package exercises03;
 // Week 3
+
 // Counting primes, using multiple threads for better performance.
 // (Much simplified from CountprimesMany.java)
 // sestoft@itu.dk * 2014-08-31, 2015-09-15
@@ -11,19 +12,20 @@ import java.util.concurrent.atomic.AtomicLong;
 
 public class TestCountPrimesThreads {
 
-  public static void main(String[] args) { new TestCountPrimesThreads(); }
+  public static void main(String[] args) {
+    new TestCountPrimesThreads();
+  }
 
   public TestCountPrimesThreads() {
     SystemInfo();
     final int range = 100_000;
 
     Mark7("countSequential", i -> countSequential(range));
-    for (int c=1; c<=32; c++) {
-    final int threadCount = c;
-    Mark7(String.format("countParallelN %7d", threadCount), 
-          i -> countParallelN(range, threadCount));
-    //Mark7(String.format("countParallelNLocal %2d", threadCount), 
-    //      i -> countParallelNLocal(range, threadCount));
+    for (int c = 1; c <= 16; c++) {
+      final int threadCount = c;
+      Mark7(String.format("countParallelN %7d", threadCount), i -> countParallelN(range, threadCount));
+      // Mark7(String.format("countParallelNLocal %2d", threadCount),
+      // i -> countParallelNLocal(range, threadCount));
     }
   }
 
@@ -38,8 +40,8 @@ public class TestCountPrimesThreads {
   private static long countSequential(int range) {
     long count = 0;
     final int from = 0, to = range;
-    for (int i=from; i<to; i++)
-      if (isPrime(i)) 
+    for (int i = from; i < to; i++)
+      if (isPrime(i))
         count++;
 
     return count;
@@ -48,31 +50,32 @@ public class TestCountPrimesThreads {
   // General parallel solution, using multiple threads
   private static long countParallelN(int range, int threadCount) {
     final int perThread = range / threadCount;
-    final LongCounter lc = new LongCounter();
+    // final LongCounter lc = new LongCounter();
+    final AtomicLong lc = new AtomicLong();
     Thread[] threads = new Thread[threadCount];
-    for (int t=0; t<threadCount; t++) {
-        final int from = perThread * t, 
-            to = (t+1==threadCount) ? range : perThread * (t+1); 
-        threads[t] = new Thread( () -> {
-                for (int i=from; i<to; i++)
-                    if (isPrime(i))
-                        lc.increment();
-            });
+    for (int t = 0; t < threadCount; t++) {
+      final int from = perThread * t, to = (t + 1 == threadCount) ? range : perThread * (t + 1);
+      threads[t] = new Thread(() -> {
+        for (int i = from; i < to; i++)
+          if (isPrime(i))
+            lc.incrementAndGet();
+      });
     }
-    for (int t=0; t<threadCount; t++) 
+    for (int t = 0; t < threadCount; t++)
       threads[t].start();
     try {
-      for (int t=0; t<threadCount; t++) 
+      for (int t = 0; t < threadCount; t++)
         threads[t].join();
-        //System.out.println("Primes: "+lc.get());
-    } catch (InterruptedException exn) { }
+      // System.out.println("Primes: "+lc.get());
+    } catch (InterruptedException exn) {
+    }
     return lc.get();
   }
 
   // General parallel solution, using multiple threads
   private static long countParallelNLocal(int range, int threadCount) {
-    //...
-    return 0; //change 0 to result;
+    // ...
+    return 0; // change 0 to result;
   }
 
   // --- Benchmarking infrastructure ---
@@ -80,62 +83,54 @@ public class TestCountPrimesThreads {
   public static double Mark6(String msg, IntToDoubleFunction f) {
     int n = 10, count = 1, totalCount = 0;
     double dummy = 0.0, runningTime = 0.0, st = 0.0, sst = 0.0;
-    do { 
+    do {
       count *= 2;
       st = sst = 0.0;
-      for (int j=0; j<n; j++) {
+      for (int j = 0; j < n; j++) {
         Timer t = new Timer();
-        for (int i=0; i<count; i++) 
+        for (int i = 0; i < count; i++)
           dummy += f.applyAsDouble(i);
         runningTime = t.check();
         double time = runningTime * 1e9 / count;
         sst += time * time;
         totalCount += count;
       }
-      double mean = st/n, sdev = Math.sqrt((sst - mean*mean*n)/(n-1));
+      double mean = st / n, sdev = Math.sqrt((sst - mean * mean * n) / (n - 1));
       System.out.printf("%-25s %15.1f ns %10.2f %10d%n", msg, mean, sdev, count);
-    } while (runningTime < 0.25 && count < Integer.MAX_VALUE/2);
+    } while (runningTime < 0.25 && count < Integer.MAX_VALUE / 2);
     return dummy / totalCount;
   }
 
   public static double Mark7(String msg, IntToDoubleFunction f) {
     int n = 10, count = 1, totalCount = 0;
     double dummy = 0.0, runningTime = 0.0, st = 0.0, sst = 0.0;
-    do { 
+    do {
       count *= 2;
       st = sst = 0.0;
-      for (int j=0; j<n; j++) {
+      for (int j = 0; j < n; j++) {
         Timer t = new Timer();
-        for (int i=0; i<count; i++) 
+        for (int i = 0; i < count; i++)
           dummy += f.applyAsDouble(i);
         runningTime = t.check();
-        double time = runningTime * 1e9 / count; 
-        st += time; 
+        double time = runningTime * 1e9 / count;
+        st += time;
         sst += time * time;
         totalCount += count;
       }
-    } while (runningTime < 0.25 && count < Integer.MAX_VALUE/2);
-    double mean = st/n, sdev = Math.sqrt((sst - mean*mean*n)/(n-1));
+    } while (runningTime < 0.25 && count < Integer.MAX_VALUE / 2);
+    double mean = st / n, sdev = Math.sqrt((sst - mean * mean * n) / (n - 1));
     System.out.printf("%-25s %15.1f ns %10.2f %10d%n", msg, mean, sdev, count);
     return dummy / totalCount;
   }
 
   public static void SystemInfo() {
-    System.out.printf("# OS:   %s; %s; %s%n", 
-                      System.getProperty("os.name"), 
-                      System.getProperty("os.version"), 
-                      System.getProperty("os.arch"));
-    System.out.printf("# JVM:  %s; %s%n", 
-                      System.getProperty("java.vendor"), 
-                      System.getProperty("java.version"));
+    System.out.printf("# OS:   %s; %s; %s%n", System.getProperty("os.name"), System.getProperty("os.version"),
+        System.getProperty("os.arch"));
+    System.out.printf("# JVM:  %s; %s%n", System.getProperty("java.vendor"), System.getProperty("java.version"));
     // The processor identifier works only on MS Windows:
-    System.out.printf("# CPU:  %s; %d \"cores\"%n", 
-                      System.getenv("PROCESSOR_IDENTIFIER"),
-                      Runtime.getRuntime().availableProcessors());
+    System.out.printf("# CPU:  %s; %d \"cores\"%n", System.getenv("PROCESSOR_IDENTIFIER"),
+        Runtime.getRuntime().availableProcessors());
     java.util.Date now = new java.util.Date();
-    System.out.printf("# Date: %s%n", 
-      new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(now));
+    System.out.printf("# Date: %s%n", new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(now));
   }
 }
-
-
